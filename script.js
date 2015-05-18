82825192afa3be6d0195c255cd136aa5bf770444
B Hold <Hotpockets01@users.noreply.github.com>
2015-05-18 11:50:35 -0400

    Update and rename script.js.test to script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+//variable test:
+var brendonHo = 12;
+var brendonJs = function(){
+rect(12,12,12,12);
+};
+var brendon = []
+//constants:
+let brendonE = 12;
+let brendonGh = function(){
+rect(12,12,12,12);
+};
